02b4183ddc66cfce28595e82cae969bbcefeacdc
jessdiv <jessica.diver@hotmail.com>
2018-12-18 10:35:25 +1100

    players now alternating

--- js/tictactoe2.js
+++ js/tictactoe2.js
@@ -3,81 +3,86 @@
 const player1 = 'X';
 const player2 = 'O';
 let currentPlayer = player1;
-const moves = 0;
-const maxMoves = 9;
 let choicesLeft = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-let play1 = []; // store id's of boxes selected
-let play2 = []; // store id's of boxes selected
+let board = [null, null, null, null, null, null, null, null, null];
 // set the winning combo's
 
-const win =[[1, 2, 3],[4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
 
 // function to switch the player
 
-const switchPlayer = function (currentPlayer) {
-  if (currentPlayer === 'X') {
-    let nextPlayer = 'O'
-    currentPlayer = nextPlayer;
-  } else {
-    let nextPlayer = 'X';
-    currentPlayer = nextPlayer;
-  }
+const switchPlayer = function () {
+  
 }
 // function to check ID against remaining choices
 
 const validMove = function (id){
-  for(let i = 0; i < 10; i++)
-  if (choicesLeft[i] === this.id){
-    return 'valid move';
+  if (this.id === 'X' || this.id === 'O'){
+    return false;
   } else {
-    return 'invalid move'
-  }
-  if (maxMoves >= 9) {
-    return 'game ended';
+    return true;
   }
 }
 
+//compares the array formed to the winning arrays.
+
 const winner = function (play) {
+  const win =[[1, 2, 3],[4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+
+
   let play = play.sort();
   for ( let i = 0; i < play.length; i ++){
-    if (play === win[i]){
+    if (play.includes[win[i]]){
       return 'winner';
     } else {
-      return;
+      return false;
     }
   }
 }
 
 // setting starting player (depends on user input)
 
-
-const startGame = function (player) {
-  let currentPlayer = player;
-}
-
 $(document).ready(function(){
 
-  // startGame(player1);
 
-  let $click = $('.box');
-  if (currentPlayer === 'X') {
-    $click.on('click', function(){
-      play1.push(this.id);
-      console.log(play1);
-      console.log('X')
-      switchPlayer(currentPlayer);
-    })
-  }
-  if (currentPlayer === 'O' ){
-    $click.on('click', function(){
-      play2.push(this.id);
-      console.log(play2);
-      console.log('O');
-      switchPlayer(currentPlayer)
-    })
-  }
+  let currentPlayer = player1;
+  let $move = $('.box');
 
-});
+    $move.on('click', function(){
+      console.log(`${ $(this).attr('id') }`);
+      if (currentPlayer === 'X') {
+        let id = $(this).attr('id');
+            board[id] = 'X';
+            $(this).text('X');
+            console.log(board);
+            currentPlayer = 'O';
+    } else {
+        let id = $(this).attr('id');
+        board[this.id] = 'O';
+        $(this).text('O');
+        console.log(board);
+        currentPlayer = 'X';
+    }
+  })
+
+
+
+
+
+  // if (currentPlayer === 'O' ){
+  //   $click.on('click', function(){
+  //     play2.push(this.id);
+  //     console.log(play2);
+  //     console.log('O');
+  //     switchPlayer(currentPlayer)
+  //   })
+  //  }
+  // }
+
+//once you click, if the currentPlayer is X, get th ID and push to board.
+
+
+
+}); //doc ready end
 
 
 // highlight the impossible outcomes.
